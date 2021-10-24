7fe90dc8a53bea40c6178145a3e2fc2142585500
Jeter Aguilar <jeteraguilar@gmail.com>
2021-10-24 09:38:34 -0300

    usuarioRepository e injeção de dependências

--- minhasfinancas/src/main/java/com/jet/minhasfinancas/model/repository/UsuarioRepository.java
+++ minhasfinancas/src/main/java/com/jet/minhasfinancas/model/repository/UsuarioRepository.java
@@ -1,9 +1,12 @@
 package com.jet.minhasfinancas.model.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jet.minhasfinancas.model.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
+	boolean existsByEmail(String email);
 }
--- minhasfinancas/src/main/java/com/jet/minhasfinancas/service/impl/UsuarioServiceImpl.java
+++ minhasfinancas/src/main/java/com/jet/minhasfinancas/service/impl/UsuarioServiceImpl.java
@@ -1,5 +1,43 @@
 package com.jet.minhasfinancas.service.impl;
 
-public class UsuarioServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jet.minhasfinancas.exception.RegraNegocioException;
+import com.jet.minhasfinancas.model.entity.Usuario;
+import com.jet.minhasfinancas.model.repository.UsuarioRepository;
+import com.jet.minhasfinancas.service.UsuarioService;
+
+@Service
+public class UsuarioServiceImpl implements UsuarioService {
+
+	private UsuarioRepository repository;	
+	
+	@Autowired
+	public UsuarioServiceImpl(UsuarioRepository repository) {
+		super();
+		this.repository = repository;
+	}
+
+	@Override
+	public Usuario autenticar(String email, String senha) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario salvarUsuario(Usuario usuario) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void validarEmail(String email) {
+		boolean existe = repository.existsByEmail(email);
+		if(existe) {
+			throw new RegraNegocioException("Já existe um usuário caadastrado com este email.");
+		}
+		
+	}
 
 }
