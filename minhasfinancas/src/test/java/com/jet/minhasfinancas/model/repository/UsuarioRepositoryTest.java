a523d4890eca0c936c17d42f3d04d3e4ebc112ec
Jeter Aguilar <jeteraguilar@gmail.com>
2021-10-27 23:26:27 -0300

    Atualizando projeto

--- minhasfinancas/src/test/java/com/jet/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ minhasfinancas/src/test/java/com/jet/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -1,31 +1,51 @@
 package com.jet.minhasfinancas.model.repository;
 
 import org.assertj.core.api.Assertions;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.jet.minhasfinancas.model.entity.Usuario;
 
 @SpringBootTest
-
+@ExtendWith(SpringExtension.class)
+@ActiveProfiles("test")
 public class UsuarioRepositoryTest {
 
-	@Autowired	
+	@Autowired
 	UsuarioRepository repository;
-	
+
 	@Test
 	public void deveVerificarAExistenciaDeUmEmail() {
-		
-		//cenário
+
+		// cenário
 		Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
 		repository.save(usuario);
-		
-		//ação/execução
+
+		// ação/execução
 		boolean result = repository.existsByEmail("usuario@email.com");
-		
-		//verificação
+
+		// verificação
 		Assertions.assertThat(result).isTrue();
+
+	}
+	
+	@Test
+	public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComOEmail() {
 		
+		// cenário		
+		repository.deleteAll();
+
+		// ação/execução
+		boolean result = repository.existsByEmail("usuario@email.com");
+
+		// verificação
+		Assertions.assertThat(result).isFalse();
+
 	}
-	}
+}
