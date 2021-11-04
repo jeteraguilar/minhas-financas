1074cf8f2470b4c028f162b72ab54157771c70a3
Jeter Aguilar <jeteraguilar@gmail.com>
2021-11-04 00:35:44 -0300

    serviços lancamento

---
+++ minhasfinancas/src/main/java/com/jet/minhasfinancas/service/LancamentoService.java
@@ -0,0 +1,16 @@
+package com.jet.minhasfinancas.service;
+
+import java.util.List;
+
+import com.jet.minhasfinancas.model.entity.Lancamento;
+import com.jet.minhasfinancas.model.enums.StatusLancamento;
+
+public interface LancamentoService {
+
+	Lancamento salvar(Lancamento lancamento);
+	Lancamento atualizar(Lancamento lancamento);
+	void deletar(Lancamento lancamento);
+	List<Lancamento> buscar(Lancamento lancamentoFiltro );
+	void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+	void validar (Lancamento lancamento);
+}
