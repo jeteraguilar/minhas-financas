8f68f72bfd1b7ae7da189ca5e2f2b928cdb92ebe
Jeter Aguilar <jeteraguilar@gmail.com>
2023-05-01 18:57:51 -0300

    make it better

--- minhasfinancas/src/main/java/com/jet/minhasfinancas/exception/RegraNegocioException.java
+++
@@ -1,8 +0,0 @@
-package com.jet.minhasfinancas.exception;
-
-public class RegraNegocioException extends RuntimeException {
-
-	public RegraNegocioException(String msg) {
-		super(msg);
-	}
-}
--- minhasfinancas/src/main/java/com/jet/minhasfinancas/model/enums/TipoLancamento.java
+++
@@ -1,7 +0,0 @@
-package com.jet.minhasfinancas.model.enums;
-
-public enum TipoLancamento {
-
-	RECEITA,
-	DESPESA
-}
